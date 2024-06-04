254b8604563d62c1566a0db20dd2517070558b4e
oswaldohernandez <oswaldohernandez@protonmail.com>
2024-06-04 14:51:09 -0600

    complete quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(number_of_apples : i32) -> i32{
+    let mut price_per_apple = 2;
+    if number_of_apples > 40 {
+        price_per_apple = 1;
+    }
+    return number_of_apples * price_per_apple;
+}
 
 // Don't modify this function!
 #[test]
